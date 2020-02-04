0265d564d51d516cb31415e02714415697a66890
Saman NahavandiPour <saman.np.1376@gmail.com>
2020-02-04 21:56:52 +0330

    navbar skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from './components/header/header.component';
+import { NavigatorComponent } from './components/navigator/navigator.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, NavigatorComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
