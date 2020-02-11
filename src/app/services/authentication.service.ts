961578331a673d1fc46689613b3af24443e31043
Saman NahavandiPour <saman.np.1376@gmail.com>
2020-02-11 17:51:43 +0330

    initial authentication service

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { User } from "../models/User";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthenticationService {
+  user?: User;
+  usersUrl: string = "api/v1/users/auth";
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(user: string, pass: string): void {
+    var body = {
+      username: user,
+      password: pass
+    };
+    this.http.post<User>(this.usersUrl, JSON.stringify(body)).subscribe(res => {
+      this.user = res;
+      //TODO : login:Observable<User>
+      console.log(res);
+    });
+  }
 }
