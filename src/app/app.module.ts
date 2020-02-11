4499ba287a16276ab11fffa43cd8883411426a9b
Saman NahavandiPour <saman.np.1376@gmail.com>
2020-02-11 17:51:43 +0330

    initial authentication service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from './components/header/header.component';
-import { NavigatorComponent } from './components/navigator/navigator.component';
-import { HomeComponent } from './components/pages/home/home.component';
-import { AboutComponent } from './components/pages/about/about.component';
-import { LoginComponent } from './components/pages/login/login.component';
-import { RegisterComponent } from './components/pages/register/register.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { NavigatorComponent } from "./components/navigator/navigator.component";
+import { HomeComponent } from "./components/pages/home/home.component";
+import { AboutComponent } from "./components/pages/about/about.component";
+import { LoginComponent } from "./components/pages/login/login.component";
+import { RegisterComponent } from "./components/pages/register/register.component";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NavigatorComponent, HomeComponent, AboutComponent, LoginComponent, RegisterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavigatorComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
