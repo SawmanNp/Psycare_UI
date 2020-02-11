71bb0bae2394886fdd8905e3f5f57208697be614
Saman NahavandiPour <saman.np.1376@gmail.com>
2020-02-11 22:37:38 +0330

    login.comp update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -21,7 +22,13 @@ import { RegisterComponent } from "./components/pages/register/register.componen
     LoginComponent,
     RegisterComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/pages/login/login.component.ts
+++ src/app/components/pages/login/login.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Validators, FormBuilder, FormGroup } from "@angular/forms";
+import { AuthenticationService } from "../../../services/authentication.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder, private auth: AuthenticationService) {
+    this.loginForm = this.fb.group({
+      username: this.fb.control(""),
+      password: this.fb.control("")
+    });
   }
 
+  ngOnInit() {}
+
+  attemptLogin() {
+    const formValue = this.loginForm.value;
+    this.auth.login(formValue.username, formValue.password);
+  }
 }
