6d9fe6ab75018031f88946dfaa6c972ebe54cb9d
Saman NahavandiPour <saman.np.1376@gmail.com>
2020-02-12 04:43:32 +0330

    navigator comp update

--- src/app/components/navigator/navigator.component.ts
+++ src/app/components/navigator/navigator.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthenticationService } from "../../services/authentication.service";
 
 @Component({
-  selector: 'app-navigator',
-  templateUrl: './navigator.component.html',
-  styleUrls: ['./navigator.component.css']
+  selector: "app-navigator",
+  templateUrl: "./navigator.component.html",
+  styleUrls: ["./navigator.component.css"]
 })
 export class NavigatorComponent implements OnInit {
-
-  constructor() { }
+  isLoggedIn: boolean;
+  constructor(private auth: AuthenticationService) {}
 
   ngOnInit() {
+    if (this.auth.user) this.isLoggedIn = true;
+    else this.isLoggedIn = false;
   }
-
 }
