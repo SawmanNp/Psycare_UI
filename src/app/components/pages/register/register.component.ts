0437e2445d3e6e2b5871116a13b0bedf0efd4eca
Saman NahavandiPour <saman.np.1376@gmail.com>
2020-02-17 00:39:53 +0330

    register service compelete

--- src/app/components/pages/register/register.component.ts
+++ src/app/components/pages/register/register.component.ts
@@ -1,29 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { RegisterService } from '../../../services/register.service';
+import { Component, OnInit } from "@angular/core";
+import { Validators, FormBuilder, FormGroup } from "@angular/forms";
+import { RegisterService } from "../../../services/register.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
   showMsg = false;
 
-  constructor(private fb: FormBuilder, private reg: RegisterService) {
+  constructor(
+    private fb: FormBuilder,
+    private reg: RegisterService,
+    private router: Router
+  ) {
     this.registerForm = this.fb.group({
-      username: this.fb.control('', [
+      username: this.fb.control("", [
         Validators.required,
         Validators.minLength(4),
-        Validators.pattern('^[A-Za-z0-9 ]+$')
+        Validators.pattern("^[A-Za-z0-9 ]+$")
       ]),
-      email: this.fb.control('', [Validators.email, Validators.required]),
-      password: this.fb.control('', [
+      email: this.fb.control("", [Validators.email, Validators.required]),
+      password: this.fb.control("", [
         Validators.required,
         Validators.minLength(4)
       ]),
-      passrepeat: this.fb.control('', [
+      passrepeat: this.fb.control("", [
         Validators.required,
         Validators.minLength(4)
       ])
@@ -40,12 +45,14 @@ export class RegisterComponent implements OnInit {
     const formValue = this.registerForm.value;
     if (formValue.password === formValue.passrepeat) {
       this.showMsg = false;
-      this.reg.register(
-        formValue.username,
-        formValue.email,
-        formValue.password
-      );
-      console.log(formValue);
+      this.reg
+        .register(formValue.username, formValue.email, formValue.password)
+        .subscribe(res => {
+          if (res["status"] == 500) {
+            console.log("register succeed!");
+            this.router.navigateByUrl("/login");
+          }
+        });
     } else {
       this.showMsg = true;
     }
