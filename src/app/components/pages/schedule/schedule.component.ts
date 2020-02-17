db1244ba20f65d9859f4accbbe9e7f095940580d
Saman NahavandiPour <saman.np.1376@gmail.com>
2020-02-17 06:10:55 +0330

    schedule.component dummy data

--- src/app/components/pages/schedule/schedule.component.ts
+++ src/app/components/pages/schedule/schedule.component.ts
@@ -16,12 +16,39 @@ export class ScheduleComponent implements OnInit {
 
   ngOnInit() {
     this.periods = [];
-    this.adv.getSchedule().subscribe(res => {
-      if (res["status"] == 200) {
-        this.periods = res["data"]["periods"];
-        console.log("schedules set");
+    // this.adv.getSchedule().subscribe(res => {
+    //   if (res["status"] == 200) {
+    //     this.periods = res["data"]["periods"];
+    //     console.log("schedules set");
+    //   }
+    // });
+    this.periods = [
+      {
+        day_of_week: 4,
+        start_time: new Date("2020-02-14T12:00:00+03:30"),
+        end_time: new Date("2020-02-14T14:30:00+03:30")
+      },
+      {
+        day_of_week: 0,
+        start_time: new Date("2020-02-14T12:00:00+03:30"),
+        end_time: new Date("2020-02-14T14:30:00+03:30")
+      },
+      {
+        day_of_week: 3,
+        start_time: new Date("2020-02-14T12:00:00+03:30"),
+        end_time: new Date("2020-02-14T14:30:00+03:30")
+      },
+      {
+        day_of_week: 2,
+        start_time: new Date("2020-02-15T12:00:00+03:30"),
+        end_time: new Date("2020-02-15T14:30:00+03:30")
+      },
+      {
+        day_of_week: 6,
+        start_time: new Date("2020-02-14T12:00:00+03:30"),
+        end_time: new Date("2020-02-14T14:30:00+03:30")
       }
-    });
+    ];
     if (typeof this.periods !== "undefined" && this.periods.length > 0) {
       this.hasSchedule = true;
       this.periods = this.periods.sort((a, b) => a.day_of_week - b.day_of_week);
