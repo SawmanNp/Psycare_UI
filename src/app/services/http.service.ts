07a2f2d290131441154293ed4dcf5cf6646b4bbb
Armin Rostami <rmin@pop-os.localdomain>
2020-02-17 04:08:20 +0330

    bug fixes

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -12,8 +12,8 @@ export class HttpService {
   getHeaders() {
     console.log(this.cookie);
     let urlHeaders = new HttpHeaders().set("Content-Type", "application/json");
-    urlHeaders.set("Accept", "application/json");
-    urlHeaders.set("Authorization", "BEARER " + this.cookie);
+    urlHeaders = urlHeaders.set("Accept", "application/json");
+    urlHeaders = urlHeaders.set("Authorization", "Bearer " + this.cookie);
     return urlHeaders;
   }
 
@@ -24,6 +24,7 @@ export class HttpService {
   }
 
   post(relativeUrl: string, data: any): Observable<any> {
+    console.log(this.getHeaders());
     return this.http.post(urls.baseUrl + relativeUrl, data, {
       headers: this.getHeaders()
     });
